952ca7847dca65145555a2490169bce616123fe7
j03-dev <24nomeniavo@gmail.com>
2024-08-14 13:44:11 +0300

    hanle error

--- app/src/main.rs
+++ app/src/main.rs
@@ -5,12 +5,12 @@ use rocket::fs::FileServer;
 use rocket_dyn_templates::Template;
 
 #[main]
-async fn main() {
+async fn main() -> Result<(), rocket::Error> {
     rocket::build()
         .attach(controllers::controller())
         .attach(Template::fairing())
         .mount("/static", FileServer::from("static"))
         .launch()
-        .await
-        .expect("failed to launch rocket instance");
+        .await?;
+    Ok(())
 }
